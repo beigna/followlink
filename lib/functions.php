aed62336b68d823e7b1585d36a8d3686ff46e33a
nachopro <nachopro@titi.nps>
2010-04-30 22:02:14 -0300

    Feature: Support for URL hits and last hit.

--- lib/functions.php
+++ lib/functions.php
@@ -10,6 +10,10 @@ function get_url($key)
     if (!$tt){$tt = tt_connect();}
 
     $data = json_decode($tt->get($key), true);
+    $data['hits'] = $data['hits'] + 1;
+    $data['last_hit'] = gmdate('Y-m-d H:i:s', time());
+
+    $tt->put($key, json_encode($data));
 
     return $data;
 }
@@ -27,7 +31,9 @@ function save_url($url)
     $data = json_encode(array(
         'url' => $url,
         'created_at' => gmdate('Y-m-d H:i:s', time()),
-        'ip' => $_SERVER['REMOTE_ADDR']
+        'ip' => $_SERVER['REMOTE_ADDR'],
+        'hits' => 0,
+        'last_hit' => '0000-00-00 00:00:00'
     ));
 
     $tt->put($key, $data);
