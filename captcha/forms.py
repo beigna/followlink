0c79c861e8a0cf786f50901126adc9d3b17f9214
nachopro <tranceway@gmail.com>
2011-12-22 11:54:02 -0300

    arreglo bobo

--- captcha/forms.py
+++ captcha/forms.py
@@ -4,6 +4,6 @@ from django.utils.safestring import mark_safe
 
 class CaptchaWidget(forms.TextInput):
     def render(self, name, value, attrs=None):
-        output = u'<img src="/captcha/add.jpg" />'
+        output = u'<img src="/captcha/%s.jpg" />' % name
         return mark_safe(output)
 
